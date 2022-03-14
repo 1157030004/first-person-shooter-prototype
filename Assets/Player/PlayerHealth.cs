eca5cabe5d9a4c8be9dc57c5269b49133d93c8f6
Ariq Dhia Irfanudin <62055087+1157030004@users.noreply.github.com>
2022-03-14 22:10:12 +0700

    game loop

--- Assets/Player/PlayerHealth.cs
+++ Assets/Player/PlayerHealth.cs
@@ -10,7 +10,7 @@ public class PlayerHealth : MonoBehaviour
         hitPoints -= damage;
         if(hitPoints <= 0)
         {
-            Debug.Log("Player is dead");
+            GetComponent<DeathHandler>().HandleDeath();
         }
 
     }
